292ed8c18ca84dfd5cd1e16d8d6efe40c8b7db77
Jordi Oltra <jordioltra2@gmail.com>
2021-01-22 16:29:11 +0100

    postgres

--- docker/CreateDB.sql
+++ docker/CreateDB.sql
@@ -2,9 +2,9 @@ CREATE TABLE Tweets (
     id serial PRIMARY KEY,
     Tweet_ID integer ,
     Person_name varchar(255) ,
-    Years integer ;
-    Salary integer ;
-    Members integer ;
+    Years integer ,
+    Salary integer ,
+    Members integer ,
     Hobby1 varchar(255) ,
     Score_Hobby1 integer ,
     Hobby2 varchar(255) ,
@@ -13,4 +13,13 @@ CREATE TABLE Tweets (
     Score_Hobby3 integer ,
     Hobby4 varchar(255) ,
     Score_Hobby4 integer 
+);
+
+CREATE TABLE Casas (
+	id serial PRIMARY KEY,
+	City_ID integer ,
+	City_Name varchar(255) , 
+	Rooms integer , 
+	Cost integer ,
+	code integer UNIC KEY
 );
